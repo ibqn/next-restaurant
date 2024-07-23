f6b18a2ee1c53b9786db711e40d0f0d59daa8f37
ibqn <evgen.ibqn@gmail.com>
2024-07-23 08:25:55 +0200

    update project structure

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/pages/**/*.{js,jsx}', './src/components/**/*.{js,jsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
