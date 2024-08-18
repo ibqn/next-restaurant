13990c9bf84c17f1d758d082cb0d07281ee7c291
ibqn <evgen.ibqn@gmail.com>
2024-08-18 21:23:31 +0200

    improve socials comp

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,3 +1,12 @@
-export const Socials = () => {
-  return <div>Socials</div>
+import { cn } from "@/utils/class-names"
+import { ComponentProps } from "react"
+
+type Props = ComponentProps<"div">
+
+export const Socials = ({ className, ...props }: Props) => {
+  return (
+    <div className={cn("", className)} {...props}>
+      Socials
+    </div>
+  )
 }
