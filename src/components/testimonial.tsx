da1ebe20dd8893809ea7de46c3af4bdd0bf60b1e
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-09-08 10:15:02 +0200

    add testimonial animation

--- src/components/testimonial.tsx
+++ src/components/testimonial.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import { testimonialData } from "@/data"
 import { TestimonialCarousel } from "@/components/testimonial-carousel"
 import Image from "next/image"
+import { motion } from "framer-motion"
+import { Direction, fadeIn, staggerContainer } from "@/utils/variants"
 
 export const Testimonial = () => {
   const { title, subtitle, modelImg, slider } = testimonialData
@@ -8,17 +12,31 @@ export const Testimonial = () => {
   return (
     <section className="relative z-10 -mt-14 min-h-[800px] bg-testimonial bg-no-repeat pt-36">
       <div className="container mx-auto">
-        <div className="flex flex-col items-center text-center capitalize">
-          <h2 className="h2 text-white">{title}</h2>
-          <p className="mb-8 capitalize text-white/70">{subtitle}</p>
-          <div className="mb-12">
-            <Image src={modelImg} alt="model" width={150} height={150} />
-          </div>
-        </div>
+        <motion.div
+          variants={staggerContainer(0.3, 1.6)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}
+          className="flex flex-col"
+        >
+          <motion.div
+            variants={fadeIn(Direction.up, "tween", 0.2, 1.6)}
+            className="flex flex-col items-center text-center capitalize"
+          >
+            <h2 className="h2 text-white">{title}</h2>
+            <p className="mb-8 capitalize text-white/70">{subtitle}</p>
+            <div className="mb-12">
+              <Image src={modelImg} alt="model" width={150} height={150} />
+            </div>
+          </motion.div>
 
-        <div className="flex items-center justify-center">
-          <TestimonialCarousel slider={slider} />
-        </div>
+          <motion.div
+            variants={fadeIn(Direction.up, "tween", 0.6, 1.6)}
+            className="flex items-center justify-center"
+          >
+            <TestimonialCarousel slider={slider} />
+          </motion.div>
+        </motion.div>
       </div>
     </section>
   )
